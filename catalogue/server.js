f878e556eab218541fdb3ccdfb8a1c79fac3ee72
prudhvi75 <m.vprudhvi7@gmail.com>
2025-11-27 12:19:30 +0530

    optimsing image

--- catalogue/server.js
+++ catalogue/server.js
@@ -6,7 +6,7 @@ instana({
         enabled: true
     }
 });
-// change to test layers,
+// change to test layers, again
 const { MongoClient, ObjectId } = require('mongodb');
 const bodyParser = require('body-parser');
 const express = require('express');
